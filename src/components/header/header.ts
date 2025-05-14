9eb3b765dea67fcaee5054e4e8704db3a804e306
Ivan <ivankonovalov96@inbox.ru>
2025-05-14 13:18:09 +0500

    refactor: change footer and header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -25,110 +25,63 @@ export default class Header {
 
     const headerNav = HtmlCreator.create('nav', undefined, 'header__nav');
     const headerList = HtmlCreator.create('ul', undefined, 'header__list', 'list-reset');
-    const headerItemCatalog = HtmlCreator.create('li', undefined, 'header__item');
-    const headerItemAbout = HtmlCreator.create('li', undefined, 'header__item');
-    const headerItemSale = HtmlCreator.create('li', undefined, 'header__item');
-    const headerItemContacts = HtmlCreator.create('li', undefined, 'header__item');
 
-    const headerItemCatalogLink = HtmlCreator.create('a', undefined, 'header__item-link');
-    headerItemCatalogLink.textContent = 'Каталог';
-    headerItemCatalogLink.setAttribute('href', '/catalog');
-    headerItemCatalogLink.addEventListener('click', (event) => {
-      const target = event.target;
+    const headerNavigation = [
+      { textLink: 'Каталог', href: '/catalog' },
+      { textLink: 'О нас', href: '/about' },
+      { textLink: 'Скидки', href: '/sales' },
+      { textLink: 'Контакты', href: '/contacts' },
+    ];
 
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
-    });
+    headerNavigation.forEach(({ textLink, href }) => {
+      const listItem = HtmlCreator.create('li', undefined, 'header__item');
+      const linkItem = HtmlCreator.create('a', undefined, 'header__item-link');
+      linkItem.textContent = textLink;
+      linkItem.setAttribute('href', href);
 
-    const headerItemAboutLink = HtmlCreator.create('a', undefined, 'header__item-link');
-    headerItemAboutLink.textContent = 'О нас';
-    headerItemAboutLink.setAttribute('href', '/about');
-    headerItemAboutLink.addEventListener('click', (event) => {
-      const target = event.target;
+      linkItem.addEventListener('click', (event) => {
+        const target = event.target;
 
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
-    });
+        if (target instanceof HTMLAnchorElement) {
+          event.preventDefault();
+          router.navigate(target.href);
+        }
+      });
 
-    const headerItemSaleLink = HtmlCreator.create('a', undefined, 'header__item-link');
-    headerItemSaleLink.textContent = 'Скидки';
-    headerItemSaleLink.setAttribute('href', '/sales');
-    headerItemSaleLink.addEventListener('click', (event) => {
-      const target = event.target;
-
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
-    });
-
-    const headerItemContactsLink = HtmlCreator.create('a', undefined, 'header__item-link');
-    headerItemContactsLink.textContent = 'Контакты';
-    headerItemContactsLink.setAttribute('href', '/contacts');
-    headerItemContactsLink.addEventListener('click', (event) => {
-      const target = event.target;
-
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
+      listItem.append(linkItem);
+      headerList.append(listItem);
     });
 
     const headerButtonWrapper = HtmlCreator.create('div', undefined, 'header__btn-wrapper');
-    const headerButtonLogin = HtmlCreator.create('a', undefined, 'header__btn', 'header__btn-login');
-    headerButtonLogin.textContent = 'Вход';
-    headerButtonLogin.setAttribute('href', '/login');
-    headerButtonLogin.addEventListener('click', (event) => {
-      const target = event.target;
 
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
-    });
+    const headerButtons = [
+      { textLink: 'Вход', href: '/login' },
+      { textLink: 'Регистрация', href: '/registration' },
+      { textLink: 'Корзина', href: '/basket' },
+    ];
 
-    const headerButtonReg = HtmlCreator.create('a', undefined, 'header__btn', 'header__btn-registration');
-    headerButtonReg.textContent = 'Регистрация';
-    headerButtonReg.setAttribute('href', '/registration');
-    headerButtonReg.addEventListener('click', (event) => {
-      const target = event.target;
+    headerButtons.forEach(({ textLink, href }) => {
+      const listItem = HtmlCreator.create('a', undefined, 'header__btn', 'header__btn-login');
+      listItem.textContent = textLink;
+      listItem.setAttribute('href', href);
 
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
-    });
+      listItem.addEventListener('click', (event) => {
+        const target = event.target;
 
-    const headerButtonBasket = HtmlCreator.create('a', undefined, 'header__btn', 'header__btn-basket');
-    headerButtonBasket.textContent = 'Корзина';
-    headerButtonBasket.setAttribute('href', '/basket');
-    headerButtonBasket.addEventListener('click', (event) => {
-      const target = event.target;
+        if (target instanceof HTMLAnchorElement) {
+          event.preventDefault();
+          router.navigate(target.href);
+        }
+      });
 
-      if (target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        router.navigate(target.href);
-      }
+      headerButtonWrapper.append(listItem);
     });
 
     this.header.append(container);
     container.append(headerWrapper);
     headerWrapper.append(headerTitleLink);
     headerWrapper.append(headerTitleLink, headerNav, headerButtonWrapper);
-
     headerNav.append(headerList);
-    headerList.append(headerItemCatalog, headerItemAbout, headerItemSale, headerItemContacts);
-
-    headerItemCatalog.append(headerItemCatalogLink);
-    headerItemAbout.append(headerItemAboutLink);
-    headerItemSale.append(headerItemSaleLink);
-    headerItemContacts.append(headerItemContactsLink);
-
-    headerButtonWrapper.append(headerButtonLogin, headerButtonReg, headerButtonBasket);
 
     return this.header;
   }
