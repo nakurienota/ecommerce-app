4a12eef89f32db94e29de1c11644a8c256621a7b
stenyukovs <s.stenyukov@mixvel.com>
2025-05-15 22:23:01 +0300

    fix: style

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,8 +2,6 @@ import { Resthandler } from '@service/rest/resthandler';
 import HtmlCreator from '@utils/html';
 import { router } from '@utils/router';
 
-import ButtonBackCreator from '../../components/button/button-back';
-
 export default class LoginPage {
   private readonly restHandler: Resthandler = Resthandler.getInstance();
   private readonly container: HTMLElement;
@@ -18,8 +16,7 @@ export default class LoginPage {
     const title: HTMLHeadingElement = HtmlCreator.create('h1', undefined, 'login__title');
     title.textContent = 'Login';
     const emailLabel: HTMLLabelElement = HtmlCreator.create('label');
-    emailLabel.textContent = 'Email';
-    emailLabel.htmlFor = 'email';
+    emailLabel.textContent = 'Login';
     const emailInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login-input-field');
     emailInput.type = 'string';
     const passwordLabel: HTMLLabelElement = document.createElement('label');
@@ -33,14 +30,13 @@ export default class LoginPage {
     const buttonLogin: HTMLButtonElement = HtmlCreator.create('button', 'submit', 'login__submit-btn');
     buttonLogin.textContent = 'LOGIN';
 
-    const buttonBack: HTMLAnchorElement = new ButtonBackCreator('BACK', ['login__btn'], undefined, '/main').render();
     form.addEventListener(
       'submit',
       (event: SubmitEvent): Promise<void> => this.handleLogin(event, emailInput.value, passwordInput.value)
     );
 
     this.container.append(loginWrapper);
-    loginWrapper.append(title, form, errorMessage, buttonBack);
+    loginWrapper.append(title, form, errorMessage);
     form.append(buttonLogin);
 
     return this.container;
