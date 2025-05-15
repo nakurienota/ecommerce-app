e75b4052bf0aaa41c31d2f399f40141b4f1b8807
stenyukovs <s.stenyukov@mixvel.com>
2025-05-15 21:28:20 +0300

    feat: login form

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import App from './pages/app';
 import './styles/main.scss';
-import { Resthandler } from './service/rest/resthandler';
 
-const restHandler = new Resthandler();
-const app = new App(restHandler);
+const app = new App();
 app.render();
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,24 +1,61 @@
+import { Resthandler } from '@service/rest/resthandler';
 import HtmlCreator from '@utils/html';
+import { router } from '@utils/router';
 
 import ButtonBackCreator from '../../components/button/button-back';
 
-export default class LogingPage {
-  public container: HTMLElement;
+export default class LoginPage {
+  private readonly restHandler: Resthandler = Resthandler.getInstance();
+  private readonly container: HTMLElement;
 
   constructor() {
     this.container = HtmlCreator.create('div', undefined, 'container');
   }
 
   public getHTML(): HTMLElement {
-    const loginWrapper = HtmlCreator.create('div', undefined, 'login', 'login__wrapper');
-    const loginText = HtmlCreator.create('p', undefined, 'login__txt');
-    loginText.textContent = 'ТУТ БУДЕТ СТРАНИЦА ВХОДА';
+    const loginWrapper: HTMLDivElement = HtmlCreator.create('div', undefined, 'login', 'login__wrapper');
+    const form: HTMLFormElement = HtmlCreator.create('form', undefined, 'login__form');
+    const title: HTMLHeadingElement = HtmlCreator.create('h1', undefined, 'login__title');
+    title.textContent = 'Login';
+    const emailLabel: HTMLLabelElement = HtmlCreator.create('label');
+    emailLabel.textContent = 'Email';
+    emailLabel.htmlFor = 'email';
+    const emailInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login-input-field');
+    emailInput.type = 'string';
+    const passwordLabel: HTMLLabelElement = document.createElement('label');
+    passwordLabel.textContent = 'Password';
+    passwordLabel.htmlFor = 'password';
+    const passwordInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login-input-field');
+    passwordInput.type = 'password';
+    form.append(emailLabel, emailInput, passwordLabel, passwordInput);
+    const errorMessage: HTMLParagraphElement = HtmlCreator.create('p', undefined, 'login__error');
 
-    const buttonBack = new ButtonBackCreator('НАЗАД', ['login__btn', 'button-back'], undefined, '/main').render();
+    const buttonLogin: HTMLButtonElement = HtmlCreator.create('button', 'submit', 'login__submit-btn');
+    buttonLogin.textContent = 'LOGIN';
+
+    const buttonBack: HTMLAnchorElement = new ButtonBackCreator('BACK', ['login__btn'], undefined, '/main').render();
+    form.addEventListener(
+      'submit',
+      (event: SubmitEvent): Promise<void> => this.handleLogin(event, emailInput.value, passwordInput.value)
+    );
 
     this.container.append(loginWrapper);
-    loginWrapper.append(loginText, buttonBack);
+    loginWrapper.append(title, form, errorMessage, buttonBack);
+    form.append(buttonLogin);
 
     return this.container;
+  }
+
+  private async handleLogin(event: Event, login: string, password: string): Promise<void> {
+    event.preventDefault();
+
+    try {
+      const token: string = await this.restHandler.getToken(login, password);
+      localStorage.setItem('auth_token', token);
+      router.navigate('/main');
+    } catch {
+      const errorMessage: Element | null = document.querySelector('.login__error');
+      if (errorMessage) errorMessage.textContent = 'Failed to login. Please check your credentials.';
+    }
   }
 }
