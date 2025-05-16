9ebedc020c5d85ee9e7fd1bddc5134d953386c26
Ivan <ivankonovalov96@inbox.ru>
2025-05-16 18:30:54 +0500

    feat: add validation for login and password

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Resthandler } from '@service/rest/resthandler';
 import HtmlCreator from '@utils/html';
-import { router } from '@utils/router';
+import { AppRoutes, router } from '@utils/router';
 
 export default class LoginPage {
   private readonly restHandler: Resthandler = Resthandler.getInstance();
@@ -14,21 +14,80 @@ export default class LoginPage {
     const loginWrapper: HTMLDivElement = HtmlCreator.create('div', undefined, 'login', 'login__wrapper');
     const form: HTMLFormElement = HtmlCreator.create('form', undefined, 'login__form');
     const title: HTMLHeadingElement = HtmlCreator.create('h1', undefined, 'login__title');
-    title.textContent = 'Login';
-    const emailLabel: HTMLLabelElement = HtmlCreator.create('label');
-    emailLabel.textContent = 'Login';
-    const emailInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login-input-field');
-    emailInput.type = 'string';
-    const passwordLabel: HTMLLabelElement = document.createElement('label');
-    passwordLabel.textContent = 'Password';
+    title.textContent = 'Войти';
+
+    const emailWrapper: HTMLDivElement = HtmlCreator.create('div', undefined, 'login__input-wrapper');
+    const emailLabel: HTMLLabelElement = HtmlCreator.create('label', undefined, 'login__label');
+    emailLabel.textContent = 'Email адрес';
+    const emailInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login__input-field');
+    emailInput.type = 'email';
+    emailInput.autocomplete = 'off';
+    const emailError: HTMLSpanElement = HtmlCreator.create('span', undefined, 'login__error', 'login__error-email');
+
+    emailInput.addEventListener('input', () => {
+      const loginValue = emailInput.value;
+      const loginError = loginValidate(loginValue);
+
+      emailError.textContent = loginError ?? '';
+
+      if (loginValue.length === 0) emailError.textContent = '';
+    });
+
+    emailInput.addEventListener('blur', () => {
+      if (emailInput.value.length === 0) emailError.textContent = '';
+    });
+
+    const passwordWrapper: HTMLDivElement = HtmlCreator.create('div', undefined, 'login__input-wrapper');
+    const passwordLabel: HTMLLabelElement = HtmlCreator.create('label', undefined, 'login__label');
+    passwordLabel.textContent = 'Пароль';
     passwordLabel.htmlFor = 'password';
-    const passwordInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login-input-field');
+    const passwordInput: HTMLInputElement = HtmlCreator.create('input', undefined, 'login__input-field');
     passwordInput.type = 'password';
-    form.append(emailLabel, emailInput, passwordLabel, passwordInput);
+    passwordInput.autocomplete = 'off';
+    const passwordError: HTMLSpanElement = HtmlCreator.create(
+      'span',
+      undefined,
+      'login__error',
+      'login__error-password'
+    );
+    const buttonShowPassword: HTMLButtonElement = HtmlCreator.create('button', undefined, 'login__btn-show');
+    const buttonShowPasswordImage = HtmlCreator.create('img', undefined, 'login__btn-img');
+    buttonShowPasswordImage.src = '../../assets/icons/show_password.svg';
+
+    passwordInput.addEventListener('input', () => {
+      const passwordValue = passwordInput.value;
+      const loginError = passwordValidate(passwordValue);
+
+      passwordError.textContent = loginError ?? '';
+
+      if (passwordValue.length === 0) passwordError.textContent = '';
+    });
+
+    passwordInput.addEventListener('blur', () => {
+      if (passwordInput.value.length === 0) passwordError.textContent = '';
+    });
+
+    buttonShowPassword.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        buttonShowPasswordImage.src = '../../assets/icons/hide_password.svg';
+      } else {
+        passwordInput.type = 'password';
+        buttonShowPasswordImage.src = '../../assets/icons/show_password.svg';
+      }
+    });
+
+    emailWrapper.append(emailLabel, emailInput, emailError);
+    passwordWrapper.append(passwordLabel, passwordInput, passwordError, buttonShowPassword);
+    buttonShowPassword.append(buttonShowPasswordImage);
+    form.append(emailWrapper, passwordWrapper);
+
     const errorMessage: HTMLParagraphElement = HtmlCreator.create('p', undefined, 'login__error');
 
     const buttonLogin: HTMLButtonElement = HtmlCreator.create('button', 'submit', 'login__submit-btn');
-    buttonLogin.textContent = 'LOGIN';
+    buttonLogin.textContent = 'Войти';
 
     form.addEventListener(
       'submit',
@@ -48,10 +107,52 @@ export default class LoginPage {
     try {
       const token: string = await this.restHandler.getToken(login, password);
       localStorage.setItem('auth_token', token);
-      router.navigate('/main');
+      router.navigate(AppRoutes.MAIN);
     } catch {
       const errorMessage: Element | null = document.querySelector('.login__error');
       if (errorMessage) errorMessage.textContent = 'Failed to login. Please check your credentials.';
     }
   }
+}
+
+function loginValidate(login: string): string | null {
+  const loginTrim = login.trim();
+
+  if (!loginTrim.includes('@')) {
+    return 'Email должен содержать символ "@"';
+  }
+
+  const [localPart, domain] = loginTrim.split('@');
+
+  if (!localPart) {
+    return 'Email должен содержать локальную часть';
+  }
+
+  if (!domain) {
+    return 'Email должен содержать доменное имя';
+  }
+
+  return null;
+}
+
+function passwordValidate(password: string): string | null {
+  const passwordTrim = password.trim();
+
+  if (passwordTrim.length < 8) {
+    return 'Пароль должен содержать не менее 8 символов';
+  }
+
+  if (!/[A-Z]/.test(passwordTrim)) {
+    return 'Пароль должен содержать хотя бы одну заглавную букву (A-Z).';
+  }
+
+  if (!/[a-z]/.test(passwordTrim)) {
+    return 'Пароль должен содержать хотя бы одну строчную букву (a-z).';
+  }
+
+  if (!/[0-9]/.test(passwordTrim)) {
+    return 'Пароль должен содержать хотя бы одну цифру (0-9).';
+  }
+
+  return null;
 }
